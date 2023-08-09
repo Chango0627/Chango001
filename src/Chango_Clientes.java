ad1900c0f69f8f7d0362b0aa52314de4bf6639ac
Chango627 <mariogregoriotestori@gmail.com>
2023-08-09 20:05:40 -0300

    dsfdsafa

---
+++ src/Chango_Clientes.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sipem
+ */
+public class Chango_Clientes {
+    private int codigo;
+    private String apellido;
+    private String domicilio;
+    private double saldo;
+
+    public Chango_Clientes(int codigo, String apellido, String domicilio, double saldo) {
+        this.codigo = codigo;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+        this.saldo = saldo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+}
